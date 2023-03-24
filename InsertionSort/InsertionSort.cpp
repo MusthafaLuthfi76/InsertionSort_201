ee6d2f0ac1353e3ea090a1c9a2aabe8515a0d074
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-03-24 10:42:47 +0700

    Menambah coment step 1 - 4 dalam prosedur insertion sort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -31,19 +31,19 @@ void input() {                  //Procedure Input
     }
 }
 
-void insertionsort() {
+void insertionsort() {                      //Prosedur InsertionSort
 
-    int temp;
-    int j;
+    int temp;                               //Membuat data temporer atau penyimpanan sementara
+    int j;                                  //membuat variabel j sebagai penanda
 
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) {           //1. Looping dengan i dimulai dari 1 hingga n-1
         
-        temp = arr[i];
+        temp = arr[i];                      //2. Simpan nilai arr[i] ke variabel sementara temp
 
-        j = i - 1;
+        j = i - 1;                          //3. Setting nilai j sama dengan i-1
 
-        while (j >= 0 && arr[j] > temp)
-
+        while (j >= 0 && arr[j] > temp)     //4. Looping while dimana nilai j lebih besar sama dengan 0 dan 
+                                            //arr[j] lebih besar daripada temp
         {
 
         }
