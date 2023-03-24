5820372b218fa1b6c77c2fe6a4d0539a3302729a
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-03-24 11:10:06 +0700

    Membenarkan typo dari a > arr

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -66,7 +66,7 @@ void display() {                            //Procedure display
     cout << "Element Array yang telah tersusun" << endl;    //Output ke layar
     cout << "====================" << endl;                 //Output ke layar
     for (int j = 0; j < n; j++) {           //Looping dengan j dimulai dari 0 hingga n-1
-        cout << a[j] << endl;               //Output ke layar
+        cout << arr[j] << endl;               //Output ke layar
     }
     cout << endl;                           //Output baris kosong
 }
